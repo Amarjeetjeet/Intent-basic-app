abf9cd89f3592806fb54a5ac695843c7635e7f9b
amar <kush.amarjeet22@gmail.com>
2022-05-20 17:43:28 +0530

    first commit of Intent project

---
+++ app/src/main/java/com/example/intent_app/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.intent_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    lateinit var etId: EditText
+    lateinit var etName: EditText
+    lateinit var etRoll: EditText
+    lateinit var btnSend: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etId = findViewById(R.id.etId)
+        etName = findViewById(R.id.etName)
+        etRoll = findViewById(R.id.etRoll)
+        btnSend = findViewById(R.id.btnSend)
+
+        btnSend.setOnClickListener {
+
+            val bundle = Bundle()
+            bundle.putString("id", etId.text.toString())
+            bundle.putString("name", etName.text.toString())
+            bundle.putString("roll", etRoll.text.toString())
+
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtras(bundle)
+            startActivity(intent)
+        }
+    }
+}
